dacfe57ba200e77905fb1dba16dd8d8541afbac0
Gabriel Rabelo <contato.ideialisa@gmail.com>
2022-09-12 10:21:33 -0300

    chore: implement graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log('Start project');
+import 'dotenv/config';
+
+enum ExitStatus {
+  Failure = 1,
+  Success = 0
+}
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error(
+    `App exiting due to an unhandled promise: ${promise} and reason: ${reason}`
+  );
+  throw reason;
+});
+
+process.on('uncaughtException', (error) => {
+  console.error(`App exiting due to an uncaught exception: ${error}`);
+  process.exit(ExitStatus.Failure);
+});
+
+(async (): Promise<void> => {
+  try {
+    console.log('starting api');
+    const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+    for (const exitSignal of exitSignals) {
+      process.on(exitSignal, async () => {
+        try {
+          console.info(`App exited with success`);
+          process.exit(ExitStatus.Success);
+        } catch (error) {
+          console.error(`App exited with error: ${error}`);
+          process.exit(ExitStatus.Failure);
+        }
+      });
+    }
+  } catch (error) {
+    console.error(`App exited with error: ${error}`);
+    process.exit(ExitStatus.Failure);
+  }
+})();
